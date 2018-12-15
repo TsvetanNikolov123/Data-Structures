138139a631d573ac19769aa4cb71b1dbeb583eda
TsvetanNikolov123 <tsdman1985@gmail.com>
2018-12-15 17:26:58 +0200

    add solution to ReversedList from exercises

--- 3 LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Exercise/src/p06ImplementDataStructureReversedListT/Main.java
+++ 3 LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Exercise/src/p06ImplementDataStructureReversedListT/Main.java
@@ -1,7 +1,29 @@
 package p06ImplementDataStructureReversedListT;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        ReversedList<Integer> reversedList = new ReversedList<>();
+
+        reversedList.add(10);
+        reversedList.add(5);
+        reversedList.add(6);
+        reversedList.add(3);
+        reversedList.add(2);
+        reversedList.add(9);
+
+        System.out.println(reversedList.capacity());
+        System.out.println(reversedList.count());
+
+        reversedList.removeAt(3);
+
+        Iterator<Integer> iterator = reversedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
--- 3 LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Exercise/src/p06ImplementDataStructureReversedListT/ReversedList.java
+++ 3 LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Exercise/src/p06ImplementDataStructureReversedListT/ReversedList.java
@@ -1,27 +1,104 @@
 package p06ImplementDataStructureReversedListT;
 
-public class ReversedList<T> {
+import java.util.Iterator;
+
+/**
+ * It gives me TIME LIMIT on every test i judge.
+ *
+ * @param <T>
+ */
+public class ReversedList<T> implements Iterable {
+    private T[] elements;
     private int size;
-    private T[] arr;
     private int capacity;
 
     public ReversedList() {
         this.size = 0;
         this.capacity = 2;
-        this.arr = (T[]) new Object[capacity];
+        this.elements = createArray(capacity);
     }
 
-    public void add(T element){
-
+    @SuppressWarnings("unchecked")
+    private static <T> T[] createArray(int capacity) {
+        return (T[]) new Object[capacity];
     }
 
-    public int count(){
-        return 0;
+    public void add(T element) {
+        if (size == capacity) {
+            capacity = capacity * 2;
+        }
+        T[] newElements = createArray(capacity);
+        for (int i = 0; i < size; i++) {
+            newElements[i] = this.elements[i];
+        }
+        newElements[size] = element;
+        size++;
+        this.elements = newElements;
     }
 
-    public int getCapacity(){
+    public T get(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        index = reverseIndex(index);
+        return this.elements[index];
+    }
+
+    public void set(int index, T element) {
+        if (index >= this.size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        index = reverseIndex(index);
+        this.elements[index] = element;
+    }
+
+    public int count() {
+        return this.size;
+    }
+
+    public int capacity() {
         return this.capacity;
     }
 
+    public void removeAt(int index) {
+        int indexToRemoveAt = reverseIndex(index);
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T element = elements[indexToRemoveAt];
+        size--;
+        for (int i = indexToRemoveAt; i < size; i++) {
+            elements[i] = elements[i + 1];
+        }
+    }
 
+    private int reverseIndex(int index) {
+        int lengthOfNotInitializedCells = this.elements.length - this.size;
+        int reversedIndex = this.elements.length - (lengthOfNotInitializedCells);
+        return reversedIndex;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new ReversedListIterator();
+    }
+
+    public class ReversedListIterator implements java.util.Iterator<T> {
+        private int internalIndex = size - 1;
+
+        @Override
+        public boolean hasNext() {
+            if (internalIndex >= 0) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            T currentElement = (T) elements[internalIndex];
+            internalIndex--;
+            return currentElement;
+        }
+    }
 }
